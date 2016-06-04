4a6d66daae7b4844eb7e0d316c3c4768f2387c02
Ait younes Mehdi Adel <overpex@gmail.com>
2016-06-04 12:26:59 -0400

    First commit for the IGS code smell correction

--- src/main/java/io/paprika/spoon/InvokMethodProcessor.java
+++ src/main/java/io/paprika/spoon/InvokMethodProcessor.java
@@ -1,17 +1,50 @@
 package io.paprika.spoon;
 
+import org.apache.log4j.Level;
 import spoon.processing.AbstractProcessor;
-import spoon.reflect.code.CtInvocation;
-import spoon.reflect.code.CtStatement;
-import spoon.support.reflect.declaration.CtMethodImpl;
+import spoon.reflect.code.*;
 
 /**
  * Reports warnings when empty catch blocks are found.
  */
-public class InvokMethodProcessor extends AbstractProcessor<CtMethodImpl> {
+public class InvokMethodProcessor extends AbstractProcessor<CtInvocation> {
+    private boolean is_getter = false;
+    private boolean is_seter = false;
 
     @Override
-    public void process(CtMethodImpl invok) {
-        System.out.println(invok.getReference().toString());
+    public boolean isToBeProcessed(CtInvocation invok) {
+        String my_igs = invok.getExecutable().toString();
+
+       if (invok.getPosition().getFile().getName().equals("Test.java"))
+        {
+
+            if (my_igs.equals("com.example.geoffrey.myapplication.Test#getVar_a()")){
+                this.is_getter = true;
+                return true;
+            }
+
+            if (my_igs.equals("com.example.geoffrey.myapplication.Test#setVar_b(int)")){
+                this.is_seter = true;
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    @Override
+    public void process(CtInvocation invok) {
+        if (is_getter){
+            //Use Expression
+            CtExpression igsGetter = getFactory().Code().createCodeSnippetExpression("var_a");
+            invok.replace(igsGetter);
+            getEnvironment().report(this, Level.WARN, invok, "INFO : GETTER on --> " + invok.getPosition());
+        }
+        else if(is_seter){
+            CtStatement igsSetter = getFactory().Code().createCodeSnippetStatement("var_b = " + invok.getArguments().get(0));
+            invok.replace(igsSetter);
+            getEnvironment().report(this, Level.WARN, invok, "INFO : SETTER on --> " + invok.getPosition());
+        }
+
     }
 }
