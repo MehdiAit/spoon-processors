3948e1ae806bee5b83a8dbb3a237c3fefe085ce5
Ait younes Mehdi Adel <overpex@gmail.com>
2016-06-10 20:12:18 -0400

    Update the Csv check with the new Csv output of Paprika

--- src/main/java/io/paprika/spoon/InvokMethodProcessor.java
+++ src/main/java/io/paprika/spoon/InvokMethodProcessor.java
@@ -3,44 +3,89 @@ package io.paprika.spoon;
 import org.apache.log4j.Level;
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.*;
+import utils.CsvReader;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by mehdi on 04/05/16
  */
 public class InvokMethodProcessor extends AbstractProcessor<CtInvocation> {
-    private boolean is_getter = false;
-    private boolean is_seter = false;
+    // Application name and methods invocation
+    private HashMap<String, ArrayList<String>> appInfo;
+    private ArrayList<String> igsName;
+    // is it a getter or setter
+    private boolean isGetter = false;
+    private boolean isSetter = false;
+
+    // Format the Csv output to get the IGS invocation and position
+    public void formatCsv(){
+        appInfo = new HashMap<>();
+        igsName = new ArrayList<>();
+        ArrayList<String> csv_reader = CsvReader.csv("igs");
+
+        for (String e : csv_reader) {
+            String [] split = e.split(",");
+            // 1; Where the IGS has been invoked - 2; The IGS invoked
+            igsName.add(split[2]);
+            appInfo.put(split[1], igsName);
+        }
+    }
+
+    // Format the Spoon methods information to .... Csv output
+    public String [] spoonFormat(String spoonMethod){
+        // Split the Spoon method name
+        return spoonMethod.split("#")[1].split("\\(|\\)");
+
+        /* Return the type if it not null (ex : setVara(int) --> int)
+        String[] split = spoonMethod.split("#")[1].split("\\(")//[1].split("\\(|\\)");
+         */
+    }
 
     @Override
     public boolean isToBeProcessed(CtInvocation invok) {
+        // Get applications information from the CSV - output
+        formatCsv();
+
+        // Get the executable in the current file
         String my_igs = invok.getExecutable().toString();
 
-       if (invok.getPosition().getFile().getName().equals("Test.java"))
-        {
+        // Get the file class name
+        String class_file = invok.getPosition().getFile().getName().split("\\.")[0];
 
-            if (my_igs.equals("com.example.geoffrey.myapplication.Test#getVar_a()")){
-                this.is_getter = true;
-                return true;
+        for (String e: appInfo.keySet()) {
+            String csvClassName = e.split("\\.")[e.split("\\.").length - 1];
+            if (class_file.equals(csvClassName)){
+
+                String [] methodName = spoonFormat(my_igs);
+                for (String f: appInfo.get(e)) {
+                    if(methodName[0].equals(f.split("#")[0])){
+                        // Is it a getter or setter ? if split.length > 1 this is a setter
+                        if (methodName.length > 1) {
+                            isSetter = true;
+                            return true;
+                        }
+                        else {
+                            isGetter = true;
+                            return true;
+                        }
+                    }
+                }
             }
-
-            if (my_igs.equals("com.example.geoffrey.myapplication.Test#setVar_b(int)")){
-                this.is_seter = true;
-                return true;
-            }
-
         }
         return false;
     }
 
     @Override
     public void process(CtInvocation invok) {
-        if (is_getter){
+        if (isGetter){
             //Use Expression
             CtExpression igsGetter = getFactory().Code().createCodeSnippetExpression("var_a");
             invok.replace(igsGetter);
             getEnvironment().report(this, Level.WARN, invok, "INFO : GETTER on --> " + invok.getPosition());
         }
-        else if(is_seter){
+        else if(isSetter){
             //Use Statement
             CtStatement igsSetter = getFactory().Code().createCodeSnippetStatement("var_b = " + invok.getArguments().get(0));
             invok.replace(igsSetter);
--- src/main/java/utils/CsvReader.java
+++ src/main/java/utils/CsvReader.java
@@ -13,16 +13,23 @@ public class CsvReader {
     public static ArrayList csv(String name){
         Scanner scanner = null;
         ArrayList<String> mim_list = new ArrayList<String>();
+
         try {
             scanner = new Scanner(new File("/home/antonin/Documents/internship/spoon/paprika-spoon-processors/"+name+".csv"));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
+        //if (name.equals("igs")) scanner.useDelimiter(",");
+
+        // Ignore the title
+        scanner.next();
         while (scanner.hasNext())
         {
             String a = scanner.next();
             mim_list.add(a);
         }
+
         scanner.close();
         return mim_list;
     }
