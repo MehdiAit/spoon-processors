d5eb90b8240a5078c494dcb4713f86c2392b9db2
Ait younes Mehdi Adel <overpex@gmail.com>
2016-06-13 15:48:32 -0400

    Fix field access for setters

--- src/main/java/io/paprika/spoon/InvokMethodProcessor.java
+++ src/main/java/io/paprika/spoon/InvokMethodProcessor.java
@@ -3,10 +3,15 @@ package io.paprika.spoon;
 import org.apache.log4j.Level;
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.*;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtMethod;
+
+import spoon.reflect.visitor.filter.AbstractFilter;
 import utils.CsvReader;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by mehdi on 04/05/16
@@ -18,6 +23,12 @@ public class InvokMethodProcessor extends AbstractProcessor<CtInvocation> {
     // is it a getter or setter
     private boolean isGetter = false;
     private boolean isSetter = false;
+
+    // Return the getter/setter name
+    private String igsInvocationName = null;
+
+    // Return the getter/setter field application
+    String getField = null;
 
     // Format the Csv output to get the IGS invocation and position
     private void formatCsv(){
@@ -64,10 +75,12 @@ public class InvokMethodProcessor extends AbstractProcessor<CtInvocation> {
                         // Is it a getter or setter ? if split.length > 1 this is a setter
                         if (methodName.length > 1) {
                             isSetter = true;
+                            igsInvocationName = methodName[0];
                             return true;
                         }
                         else {
                             isGetter = true;
+                            igsInvocationName = methodName[0];
                             return true;
                         }
                     }
@@ -81,14 +94,29 @@ public class InvokMethodProcessor extends AbstractProcessor<CtInvocation> {
     public void process(CtInvocation invok) {
         if (isGetter){
             //Use Expression
-            CtExpression igsGetter = getFactory().Code().createCodeSnippetExpression("var_a");
-            invok.replace(igsGetter);
+            //CtExpression igsGetter = getFactory().Code().createCodeSnippetExpression("var_a");
+            //invok.replace(igsGetter);
+
             isGetter = false;
             getEnvironment().report(this, Level.WARN, invok, "INFO : GETTER on --> " + invok.getPosition());
         }
         else if(isSetter){
-            //Use Statement
-            CtStatement igsSetter = getFactory().Code().createCodeSnippetStatement("var_b = " + invok.getArguments().get(0));
+            // Get the root class
+            CtClass root = (CtClass)invok.getParent().getParent().getParent();
+            // Get the IGS or Setter
+            CtMethod method = (CtMethod) root.getMethodsByName(igsInvocationName).get(0);
+
+            // Filter to get the field of the getter/setter
+            method.getBody().getLastStatement().getElements(new AbstractFilter<CtAssignment>(CtAssignment.class) {
+                @Override
+                public boolean matches(CtAssignment element) {
+                    getField = element.getAssignment().toString();
+                    return super.matches(element);
+                }
+            });
+
+            //Use CtStatement for code transformation
+            CtStatement igsSetter = getFactory().Code().createCodeSnippetStatement(getField + " = " + invok.getArguments().get(0));
             invok.replace(igsSetter);
             isSetter = false;
             getEnvironment().report(this, Level.WARN, invok, "INFO : SETTER on --> " + invok.getPosition());
