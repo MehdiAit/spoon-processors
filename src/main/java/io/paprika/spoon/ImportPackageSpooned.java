f62976a9a7f930c257a45422c894abdbb5582253
Ait younes Mehdi Adel <overpex@gmail.com>
2016-07-14 09:23:16 -0400

    Improve import processor to also handle Interface

--- src/main/java/io/paprika/spoon/ImportPackageSpooned.java
+++ src/main/java/io/paprika/spoon/ImportPackageSpooned.java
@@ -2,6 +2,8 @@ package io.paprika.spoon;
 
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtInterface;
+import spoon.reflect.declaration.CtType;
 
 
 import java.io.*;
@@ -13,7 +15,7 @@ import java.util.HashMap;
  * Created by Mehdi on 16-07-13.
  */
 
-public class ImportPackageSpooned extends AbstractProcessor<CtClass>{
+public class ImportPackageSpooned extends AbstractProcessor<CtType>{
     int count = 0;
     HashMap<String, ArrayList<String>> getImport;
 
@@ -22,56 +24,55 @@ public class ImportPackageSpooned extends AbstractProcessor<CtClass>{
     }
 
     @Override
-    public void process(CtClass element) {
-        String [] filePath = element.getQualifiedName().split("\\$");
-        if(filePath.length > 1){
-            return;
-        }
+    public void process(CtType element) {
+        if ((element instanceof CtClass) || (element instanceof CtInterface)) {
+            String[] filePath = element.getQualifiedName().split("\\$");
+            if (filePath.length > 1) {
+                return;
+            }
 
-        if (getImport.containsKey(filePath[0])){
-            ArrayList<String> classImport = getImport.get(filePath[0]);
-            ArrayList<String> classFile = new ArrayList<>();
-            String packageName = "";
-            count++;
+            if (getImport.containsKey(filePath[0])) {
+                ArrayList<String> classImport = getImport.get(filePath[0]);
+                ArrayList<String> classFile = new ArrayList<>();
+                String packageName = "";
+                count++;
 
-            try {
-                BufferedReader readFile = new BufferedReader(new FileReader(element.getPosition().getFile()));
-                String line = "";
-                while ((line = readFile.readLine()) != null){
-                    classFile.add(line);
-                }
-
-                ArrayList<String> classFileTmp = new ArrayList<>(classFile);
-                for (String p : classFileTmp)
-                {
-                    if(p.matches("package.*")){
-                        packageName = p;
-                        classFile.remove(classFile.indexOf(p));
+                try {
+                    BufferedReader readFile = new BufferedReader(new FileReader(element.getPosition().getFile()));
+                    String line = "";
+                    while ((line = readFile.readLine()) != null) {
+                        classFile.add(line);
                     }
-                }
 
-                BufferedWriter writer = new BufferedWriter(new FileWriter(element.getPosition().getFile()));
+                    ArrayList<String> classFileTmp = new ArrayList<>(classFile);
+                    for (String p : classFileTmp) {
+                        if (p.matches("package.*")) {
+                            packageName = p;
+                            classFile.remove(classFile.indexOf(p));
+                        }
+                    }
 
-                // Write the package name
-                writer.write(packageName);
-                writer.newLine();
+                    BufferedWriter writer = new BufferedWriter(new FileWriter(element.getPosition().getFile()));
 
-                for(String s : classImport)
-                {
-                    writer.write(s);
+                    // Write the package name
+                    writer.write(packageName);
                     writer.newLine();
-                }
 
-                // Re-Write the file
-                for(String s : classFile)
-                {
-                    writer.write(s);
-                    writer.newLine();
-                }
+                    for (String s : classImport) {
+                        writer.write(s);
+                        writer.newLine();
+                    }
 
-                writer.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+                    // Re-Write the file
+                    for (String s : classFile) {
+                        writer.write(s);
+                        writer.newLine();
+                    }
+
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
--- src/main/java/io/paprika/spoon/ImportPackages.java
+++ src/main/java/io/paprika/spoon/ImportPackages.java
@@ -10,42 +10,46 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import spoon.Launcher;
+import spoon.reflect.declaration.CtInterface;
+import spoon.reflect.declaration.CtType;
 
 
 /**
  * Created by Mehdi on 16-07-13.
  */
-public class ImportPackages extends AbstractProcessor<CtClass> {
+public class ImportPackages extends AbstractProcessor<CtType> {
     int count = 0;
     HashMap<String, ArrayList<String>> appImport = new HashMap<>();
 
     @Override
-    public void process(CtClass element) {
-        String [] filePath = element.getQualifiedName().split("\\$");
-        ArrayList<String> importList = new ArrayList<>();
+    public void process(CtType element) {
+        if ((element instanceof CtClass) || (element instanceof CtInterface)) {
+            String[] filePath = element.getQualifiedName().split("\\$");
+            ArrayList<String> importList = new ArrayList<>();
 
-        if(filePath.length > 1){
-            return;
-        }
+            if (filePath.length > 1) {
+                return;
+            }
 
-        try {
-            BufferedReader readFile = new BufferedReader(new FileReader(element.getPosition().getFile()));
             try {
+                BufferedReader readFile = new BufferedReader(new FileReader(element.getPosition().getFile()));
+                try {
 
-                String line = "";
-                while ((line = readFile.readLine()) != null){
-                    if(line.matches("import.*")){
-                        importList.add(line);
+                    String line = "";
+                    while ((line = readFile.readLine()) != null) {
+                        if (line.matches("import.*")) {
+                            importList.add(line);
+                        }
                     }
-                }
-                appImport.put(filePath[0],importList);
-                count++;
+                    appImport.put(filePath[0], importList);
+                    count++;
 
-            } catch (IOException e) {
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         }
     }
 
